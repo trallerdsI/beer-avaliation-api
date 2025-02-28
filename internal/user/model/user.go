6d013b57718c7dc6dba17b54879deef7369d50cd
trallerds <jessicacristec@gmail.com>
2025-02-28 16:45:39 -0300

    fix: user creation

--- internal/user/model/user.go
+++ internal/user/model/user.go
@@ -4,6 +4,6 @@ type User struct {
 	ID       string `json:"id"`
 	Username string `json:"username" validate:"required,min=3,max=50"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"-" validate:"required,min=6"` // "-" means don't show in JSON
-	Created  string `json:"created"`
+	Password string `json:"password" validate:"required,min=6"` // "-" means don't show in JSON
+	Created  string `json:"-"`
 }
