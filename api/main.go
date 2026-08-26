88b62afe5f153c0f4707b8add54f700eb3f2963a
trallerds <jessicacristec@gmail.com>
2026-08-26 11:07:43 -0300

    feat(deploy): add Vercel serverless entrypoint and config

---
+++ api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"beer-review-app/internal/app"
+	"beer-review-app/pkg/logging"
+)
+
+var router http.Handler
+
+func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, logging.SanitizeOptions(&slog.HandlerOptions{Level: slog.LevelInfo})))
+	db, _ := app.InitDBFromEnv()
+	if db != nil {
+		router = app.BuildRouter(db, logger)
+	} else {
+		router = app.BuildRouter(nil, logger)
+	}
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
+}
+
+func main() {}
